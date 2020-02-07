b44ad94d20c56ee3b224ec912ad17ff5123f91af
matheus <47143402+matheussmoura@users.noreply.github.com>
2020-02-07 11:29:25 -0300

    Add files via upload

--- login.php
+++ login.php
@@ -1,111 +1,67 @@
-<?php 
-session_start();
-include('conexao.php');
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Acesso Restrito</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+ 
+    <link href="css/styles.css" rel="stylesheet">
 
-?>
-<?php 
-include 'menu.php'
-?>
+  
+  </head>
+
+  
+  <body class="login-bg">
+  	<div class="header">
+	     <div class="container">
+	        <div class="row">
+	           <div class="col-md-12">
+	              <!-- Logo -->
+	              <div class="logo">
+	                 <h1><a href="login.php">SISTEMA</h1></a>
+	              </div>
+	           </div>
+	        </div>
+	     </div>
+	</div>
+
+	
+	<div class="page-content container">
+		<div class="row">
+			<div class="col-md-4 col-md-offset-4">
+				<div class="login-wrapper">
+			        <div class="box">
+			            <div class="content-wrap">
+							
+							<br/>
+							<br/>
+			                <h3>ACESSO RESTRITO</h3>
+			               <br/>
+				<form class="form-vertical" method="post" action="restrito-autenticar.php">			
+			                <input class="form-control" type="text" name="email" id="email"  placeholder="Usuário">
+			                <input class="form-control" type="password" name="senha" id="senha" placeholder="Senha">
+			                <div class="action">
+			                    <button type="submit" class="btn btn-primary">Entrar</button>
+								<a href="../index.php" class="btn btn-primary" value="Home">Home</a>
+			                </div>   
+						</form>
+								
+			            </div>
+			        </div>
+
+			        
+			    </div>
+			</div>
+		</div>
+	</div>
 
 
 
-<div class="container">
-
-<div id="login">
-        <h3 class="text-center text-white pt-5">Psymed</h3>
-        <div class="container">
-            <div id="login-row" class="row justify-content-center align-items-center">
-                <div id="login-column" class="col-md-6">
-                    <div id="login-box" class="col-md-12">
-                        <form id="login-form" class="form" action="fazlogin.php" method="post">
-                            <h3 class="text-center text-info">Login</h3>
-                            <div class="form-group">
-                                <label for="email" class="text-info">E-mail:</label><br>
-                                <input type="text" name="email" id="email" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="password" class="text-info">Senha:</label><br>
-                                <input type="password" name="senha" id="password" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="remember-me" class="text-info"><span>Me lembre</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
-                                <input type="submit" name="submit" class="btn btn-info btn-md" value="Entrar">
-                            </div>
-                            <div id="register-link" class="text-right">
-                                <a href="cadastro.php" class="text-info">Cadastre aqui</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-<footer class="section footer-section">
-      <div class="container">
-        <div class="row mb-4">
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-              <li><a href="#">Sobre nós</a></li>
-              <li><a href="#">Termos &amp; usos</a></li>
-              <li><a href="#">Politica de privacidade</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5">
-            <ul class="list-unstyled link">
-                <li><a href="#">Sobre nós</a></li>
-              <li><a href="contato.php">Fale conosco</a></li>
-            </ul>
-          </div>
-          <div class="col-md-3 mb-5 pr-md-5 contact-info">
-            <!-- <li>198 West 21th Street, <br> Suite 721 New York NY 10016</li> -->
-            <p><span class="d-block"><span class="ion-ios-location h5 mr-3 text-primary"></span>Endereço:</span> <span> 198 West 21th Street, <br> Suite 721 New York NY 10016</span></p>
-            <p><span class="d-block"><span class="ion-ios-telephone h5 mr-3 text-primary"></span>Telefone:</span> <span> (+1) 435 3533</span></p>
-            <p><span class="d-block"><span class="ion-ios-email h5 mr-3 text-primary"></span>Email:</span> <span> info@domain.com</span></p>
-          </div>
-        </div>
-        <div class="row pt-5">
-          <p class="col-md-6 text-left">
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Direitos autorais &copy;<script>document.write(new Date().getFullYear());</script> Todos os direitos reservados | Esse template é feito com <i class="icon-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
-            <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-          </p>
-            
-          <p class="col-md-6 text-right social">
-            <a href="#"><span class="fa fa-tripadvisor"></span></a>
-            <a href="#"><span class="fa fa-facebook"></span></a>
-            <a href="#"><span class="fa fa-twitter"></span></a>
-            <a href="#"><span class="fa fa-linkedin"></span></a>
-            <a href="#"><span class="fa fa-vimeo"></span></a>
-          </p>
-        </div>
-      </div>
-    </footer>
-    
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/jquery.fancybox.min.js"></script>
-    
-    
-    <script src="js/aos.js"></script>
-    
-    <script src="js/bootstrap-datepicker.js"></script> 
-    <script src="js/jquery.timepicker.min.js"></script> 
-
-    
-
-    <script src="js/main.js?v=0.1"></script>
-
-    <!-- JavaScript (Opcional) -->
-    <!-- jQuery primeiro, depois Popper.js, depois Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="js/custom.js"></script>
   </body>
 </html>
